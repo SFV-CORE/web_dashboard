365d2b7eb59df952b7b04d098e9d8377934dd157
Brisado2P <vg342093@gmail.com>
2021-09-22 19:29:31 -0300

    Top Navigation Bar

--- lib/layout.dart
+++ lib/layout.dart
@@ -3,23 +3,23 @@ import 'package:web_dashboard/helpers/responsivenees.dart';
 import 'package:web_dashboard/widgets/large_screen.dart';
 import 'package:web_dashboard/widgets/medium_screen.dart';
 import 'package:web_dashboard/widgets/small_screen.dart';
+import 'package:web_dashboard/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //define a parte de cima do site
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-        ),
+        //*define a parte de cima do site
+        key: scaffoldKey,
+        appBar: topNavigationBar(context, scaffoldKey),
+        drawer: Drawer(),
         // ignore: prefer_const_constructors
         body: ResponsiveWidget(
           largeScreen: const LargeScreen(),
           smallScreen: const SmallScreen(),
-          mediumScreen: MediumScreen(),
+          mediumScreen: const MediumScreen(),
         ));
   }
 }
